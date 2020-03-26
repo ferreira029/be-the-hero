38ced1d01aae61cd5995abd592cc735cf3a1a908
Marc Ferreira <msoftware.marc@outlook.com>
2020-03-26 11:06:18 -0300

    FrontEnd do App

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+import api from './../../services/api';
+
+import "./styles.css";
+import heroesImg from './../../assets/heroes.png';
+import logoImg from './../../assets/logo.svg';
+
+export default function Logon() {
+
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    const handleLogon = async event => {
+        event.preventDefault();
+
+        try {
+            const result = await api.post("session", { id });
+
+            sessionStorage.setItem("ong_id", id);
+            sessionStorage.setItem("ong_name", result.data.name);
+
+            history.push('/profile');
+
+        } catch (error) {
+            alert('Falha no Login')
+        }
+
+    }
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="heroes"/>
+
+                <form onSubmit={handleLogon}>
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="Sua ID" value={id} onChange={event => setId(event.target.value)} />
+                    <button className="button" type="submit">Entrar</button>
+                    <Link className="back-link" to="/register"> <FiLogIn size={16} color="#e02041" /> Não tenho cadastro </Link>
+                </form>
+            </section>
+            <img src={heroesImg} alt="heroes"/>
+        </div>
+    )
+}
